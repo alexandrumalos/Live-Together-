e9ac5f959e10e9cd4f1aa672ac3624b85a86f8d4
JB <jibirt@valdosta.edu>
2016-02-28 23:03:47 +0000

    initial events scaffold

---
+++ db/migrate/20160228225139_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.description :description
+      t.date :date
+      t.string :location
+
+      t.timestamps null: false
+    end
+  end
+end
