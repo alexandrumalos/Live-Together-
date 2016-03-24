9439c2560ecfa41569af82a1690875246dedd6d6
Alex <amalos@valdosta.edu>
2016-03-24 07:28:15 -0400

    Installed and configured devise and forem

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@
 #may can use has_and_belongs_to_many :groups :neighborhoods
 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :groups
   has_and_belongs_to_many :neighborhoods
   has_many :requests, dependent: :destroy
