9e20e1dad8a22467f058179f9529c8e28858ffcf
Ben Watson <americk0@gmail.com>
2016-03-31 01:41:31 -0400

    removes the 'password_digest' field from User
    it is no longer needed since devise/forem has its own
    'encrypted_password' field that it added to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@
 #
 #  id                     :integer          not null, primary key
 #  username               :string
-#  password_digest        :string
 #  name                   :string
 #  isNewser               :integer
 #  created_at             :datetime         not null
@@ -14,14 +13,14 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  sign_in_count          :integer          default(0), not null
+#  sign_in_count          :integer          default("0"), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string
 #  last_sign_in_ip        :string
-#  forem_admin            :boolean          default(FALSE)
+#  forem_admin            :boolean          default("f")
 #  forem_state            :string           default("pending_review")
-#  forem_auto_subscribe   :boolean          default(FALSE)
+#  forem_auto_subscribe   :boolean          default("f")
 #
 
 #may can use has_and_belongs_to_many :groups :neighborhoods
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331032818) do
+ActiveRecord::Schema.define(version: 20160331053248) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -206,7 +206,6 @@ ActiveRecord::Schema.define(version: 20160331032818) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
-    t.string   "password_digest"
     t.string   "name"
     t.integer  "isNewser"
     t.datetime "created_at",                                        null: false
