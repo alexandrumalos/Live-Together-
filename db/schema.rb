cfed75440dbf4ae69bc05b2d415b5a46cd8b5cb4
JB <jibirt@valdosta.edu>
2016-04-12 23:58:21 +0000

    added tab panel to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -239,18 +239,21 @@ ActiveRecord::Schema.define(version: 20160412052752) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "name"
-    t.datetime "created_at",                           null: false
-    t.datetime "updated_at",                           null: false
-    t.string   "email",                   default: "", null: false
-    t.string   "encrypted_password",      default: "", null: false
+    t.datetime "created_at",                                         null: false
+    t.datetime "updated_at",                                         null: false
+    t.string   "email",                   default: "",               null: false
+    t.string   "encrypted_password",      default: "",               null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,  null: false
+    t.integer  "sign_in_count",           default: 0,                null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "forem_admin",             default: false
+    t.string   "forem_state",             default: "pending_review"
+    t.boolean  "forem_auto_subscribe",    default: false
     t.integer  "current_neighborhood_id"
     t.string   "type"
     t.integer  "score"
