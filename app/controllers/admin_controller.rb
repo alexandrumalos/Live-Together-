3ac46918b5ac9b3c4528e7ef4c3b8839e788e924
Alex <amalos@valdosta.edu>
2016-04-20 16:55:48 -0400

    Defined Admin Index so only if user is admin they will be able to see the page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,11 @@
 class AdminController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
+    if current_user.user_type != 'admin'
+      respond_to do |format|
+        format.html { redirect_to root_url }
+      end
   end
+
 end
