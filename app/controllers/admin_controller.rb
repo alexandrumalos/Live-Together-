6339eda44ad988b20465f473072f05ad84b91cf2
Alex <amalos@valdosta.edu>
2016-04-21 12:11:43 -0400

    Styled Users table in Admin view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,14 +2,13 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
     @users = User.all
-  end
-  
-#   def index
-#     if current_user.user_type != 'admin'
-#       respond_to do |format|
-#         format.html { redirect_to root_url }
-#       end
-#     end
-# end
+
+    if current_user.user_type != 'admin'
+      respond_to do |format|
+        format.html { redirect_to root_url }
+      end
+    end
+ end
 end
