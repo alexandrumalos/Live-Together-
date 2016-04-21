3bff2563f92ed2f62ddb230c5271378042c16697
Ben Watson <americk0@gmail.com>
2016-04-20 20:14:34 -0400

    added delete button for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -43,17 +43,18 @@ class CategoriesController < ApplicationController
 
       respond_to do |format|
         if @category.save
-          format.html { redirect_to @category, notice: 'Category was successfully created.' }
+          format.html { redirect_to current_user.current_neighborhood, notice: 'Category was successfully created.' }
           format.json { render :show, status: :created, location: @category }
         else
           format.html { render :new }
           format.json { render json: @category.errors, status: :unprocessable_entity }
         end
-      else
-        respond_to do |format|
-          format.html { redirect_to current_user.current_neighborhood, notice: 'Only leads can create categories' }
-        end
       end
+    else
+      respond_to do |format|
+        format.html { redirect_to current_user.current_neighborhood, notice: 'Only leads can create categories' }
+      end
+    end
   end
 
   # PATCH/PUT /categories/1
@@ -73,9 +74,10 @@ class CategoriesController < ApplicationController
   # DELETE /categories/1
   # DELETE /categories/1.json
   def destroy
+    neighborhood = @category.neighborhood
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to neighborhood, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
