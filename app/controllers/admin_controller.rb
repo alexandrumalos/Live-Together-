a41bfd08170439ad2c48581c0a07aa046e238681
Alex <amalos@valdosta.edu>
2016-04-21 12:09:06 -0400

    Added ability to see all site users

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,14 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.user_type != 'admin'
-      respond_to do |format|
-        format.html { redirect_to root_url }
-      end
-    end
-end
+    @users = User.all
+  end
+  
+#   def index
+#     if current_user.user_type != 'admin'
+#       respond_to do |format|
+#         format.html { redirect_to root_url }
+#       end
+#     end
+# end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,31 @@ ActiveRecord::Schema.define(version: 20160421095216) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "impressions", force: :cascade do |t|
+    t.string   "impressionable_type"
+    t.integer  "impressionable_id"
+    t.integer  "user_id"
+    t.string   "controller_name"
+    t.string   "action_name"
+    t.string   "view_name"
+    t.string   "request_hash"
+    t.string   "ip_address"
+    t.string   "session_hash"
+    t.text     "message"
+    t.text     "referrer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "impressions", ["controller_name", "action_name", "ip_address"], name: "controlleraction_ip_index"
+  add_index "impressions", ["controller_name", "action_name", "request_hash"], name: "controlleraction_request_index"
+  add_index "impressions", ["controller_name", "action_name", "session_hash"], name: "controlleraction_session_index"
+  add_index "impressions", ["impressionable_type", "impressionable_id", "ip_address"], name: "poly_ip_index"
+  add_index "impressions", ["impressionable_type", "impressionable_id", "request_hash"], name: "poly_request_index"
+  add_index "impressions", ["impressionable_type", "impressionable_id", "session_hash"], name: "poly_session_index"
+  add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index"
+  add_index "impressions", ["user_id"], name: "index_impressions_on_user_id"
+
   create_table "messages", force: :cascade do |t|
     t.string   "text"
     t.string   "head"
@@ -93,16 +118,6 @@ ActiveRecord::Schema.define(version: 20160421095216) do
   end
 
   add_index "neighborhoods", ["parent_id"], name: "index_neighborhoods_on_parent_id"
-
-  create_table "pending_events", force: :cascade do |t|
-    t.integer  "pending_event"
-    t.integer  "pending_neighborhood"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
-
-  add_index "pending_events", ["pending_event"], name: "index_pending_events_on_pending_event"
-  add_index "pending_events", ["pending_neighborhood"], name: "index_pending_events_on_pending_neighborhood"
 
   create_table "posts", force: :cascade do |t|
     t.text     "body"
@@ -164,21 +179,18 @@ ActiveRecord::Schema.define(version: 20160421095216) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "name"
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
-    t.string   "email",                   default: "",               null: false
-    t.string   "encrypted_password",      default: "",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "email",                   default: "", null: false
+    t.string   "encrypted_password",      default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,                null: false
+    t.integer  "sign_in_count",           default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "forem_admin",             default: false
-    t.string   "forem_state",             default: "pending_review"
-    t.boolean  "forem_auto_subscribe",    default: false
     t.integer  "current_neighborhood_id"
     t.integer  "score"
     t.string   "phone_number"
