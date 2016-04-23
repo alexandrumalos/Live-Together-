ec22df50db50f5d0ff7c82a94165b512302b1c80
Alex <amalos@valdosta.edu>
2016-04-23 09:58:29 -0400

    Added more styling to messages

--- db/schema.rb
+++ db/schema.rb
@@ -120,16 +120,6 @@ ActiveRecord::Schema.define(version: 20160422205813) do
 
   add_index "neighborhoods", ["parent_id"], name: "index_neighborhoods_on_parent_id"
 
-  create_table "pending_events", force: :cascade do |t|
-    t.integer  "pending_event"
-    t.integer  "pending_neighborhood"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
-  end
-
-  add_index "pending_events", ["pending_event"], name: "index_pending_events_on_pending_event"
-  add_index "pending_events", ["pending_neighborhood"], name: "index_pending_events_on_pending_neighborhood"
-
   create_table "posts", force: :cascade do |t|
     t.text     "body"
     t.integer  "score"
@@ -190,21 +180,18 @@ ActiveRecord::Schema.define(version: 20160422205813) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "name"
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
-    t.string   "email",                   default: "",               null: false
-    t.string   "encrypted_password",      default: "",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.string   "email",                   default: "", null: false
+    t.string   "encrypted_password",      default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,                null: false
+    t.integer  "sign_in_count",           default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.boolean  "forem_admin",             default: false
-    t.string   "forem_state",             default: "pending_review"
-    t.boolean  "forem_auto_subscribe",    default: false
     t.integer  "current_neighborhood_id"
     t.integer  "score"
     t.string   "phone_number"
