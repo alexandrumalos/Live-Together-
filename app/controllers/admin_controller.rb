6047fca12a0760f4ec26e471c5b1c08259f499cf
Ben Watson <americk0@gmail.com>
2016-04-25 01:12:23 -0400

    buffs up the admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,18 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
-    @users = User.all
-
     if current_user.user_type != 'admin'
       respond_to do |format|
         format.html { redirect_to root_url }
       end
     end
- end
+
+    @users = User.all.order(:name)
+    @user_count = @users.count
+    @users = @users.paginate(page: params[:user_page], per_page:20)
+
+    @neighborhoods = Neighborhood.all.order(:name)
+    @neighborhood_count = @neighborhoods.count
+    @neighborhoods = @neighborhoods.paginate(page: params[:neighborhood_pag], per_page:20)
+  end
 end
